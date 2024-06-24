b1a3c64f56f5ad10a9a52171e3936665bf308091
Pamela <pamela_borges18@hotmail.com>
2024-06-24 12:14:17 -0300

    feat: create event endpoint

---
+++ src/main/java/com/eventostech/api/controller/EventController.java
@@ -0,0 +1,31 @@
+package com.eventostech.api.controller;
+
+import com.eventostech.api.domain.event.Event;
+import com.eventostech.api.domain.event.EventsRequestDTO;
+import com.eventostech.api.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Event> create(@RequestParam("title") String title,
+                                        @RequestParam(value = "description", required = false) String description,
+                                        @RequestParam("date") Long date,
+                                        @RequestParam("city") String city,
+                                        @RequestParam("state") String state,
+                                        @RequestParam("remote") Boolean remote,
+                                        @RequestParam("eventUrl") String eventUrl,
+                                        @RequestParam(value = "image", required = false) MultipartFile image) {
+        EventsRequestDTO eventRequestDTO = new EventsRequestDTO(title, description, date, city, state, remote, eventUrl, image);
+        Event newEvent = this.eventService.createEvent(eventRequestDTO);
+        return ResponseEntity.ok(newEvent);
+    }
+}
